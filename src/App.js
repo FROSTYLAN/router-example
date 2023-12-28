f496ceccf3838997a1a18bdae7e482cf93fe1edb
Charles Castillo <castillo089frosty@gmail.com>
2023-12-28 10:36:12 -0500

    useParams: rutas dinámicas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { BlogPage } from "./pages/BlogPage";
 import { ProfilePage } from "./pages/ProfilePage";
+import { BlogPost } from "./components/BlogPost";
 import { Menu } from "./components/Menu";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<p>Not found</p>} />
         </Routes>
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { blogdata } from "../api/blogdata";
 
 function BlogPage() {
-  return <h1>BlogPage</h1>;
+  return (
+    <>
+      <h1>BlogPage</h1>;
+
+      <ul>
+        {
+          blogdata.map(post => (
+            <BlogLink post={post} />
+          ))
+        }
+      </ul>
+    </>
+  )
+}
+
+function BlogLink({ post }) {
+  return (
+    <li>
+      <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    </li>
+  )
 }
 
 export { BlogPage };

f9b0872d83af1d526d3ce94ac8fec92709e62585
Charles Castillo <castillo089frosty@gmail.com>
2024-01-01 15:14:40 -0500

    useAuth: login y logout

--- src/App.js
+++ src/App.js
@@ -5,26 +5,34 @@ import { BlogPage } from "./pages/BlogPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { BlogPost } from "./components/BlogPost";
 import { Menu } from "./components/Menu";
+import { LoginPage } from "./pages/LoginPage";
+import { LogoutPage } from "./pages/LogoutPage";
+import { AuthProvider } from "./api/auth";
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Menu />
+        <AuthProvider>
+          <Menu />
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-          <Route path="/blog" element={<BlogPage />}>
-            <Route path=":slug" element={<BlogPost />} />
-          </Route>
+            <Route path="/blog" element={<BlogPage />}>
+              <Route path=":slug" element={<BlogPost />} />
+            </Route>
 
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<p>Not found</p>} />
-        </Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogoutPage />} />
+
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
-} 
+}
 
 export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,38 +5,29 @@ function Menu() {
   return (
     <nav>
       <ul>
-        {
-          routes.map(route => (
-            <li>
-              <NavLink 
-                key={route.to}
-                style={({ isActive }) => ({
-                  color: isActive ? 'red' : 'blue'
-                })}
-                to={route.to}
-              >
-                {route.text}
-              </NavLink>
-            </li>
-          ))
-        }
+        {routes.map(route => (
+          <li key={route.to}>
+            <NavLink 
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue'
+              })}
+              to={route.to}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   )
 }
 
-const routes = []
-routes.push({
-  to: '/',
-  text: 'Home'
-})
-routes.push({
-  to: '/blog',
-  text: 'Blog'
-})
-routes.push({
-  to: '/profile',
-  text: 'Home'
-})
+const routes = [
+  { to: '/', text: 'Home' },
+  { to: '/blog', text: 'Blog' },
+  { to: '/profile', text: 'Profile' }, 
+  { to: '/login', text: 'Login' },
+  { to: '/logout', text: 'Logout' }
+];
 
 export { Menu };
